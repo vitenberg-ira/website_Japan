eca6d843aa4d9eb0a7a115e5674c54288c0e10d7
Ira <i.a.ermolova@mail.ru>
2023-09-17 01:40:00 +0300

    Updated drop-down list

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const header = document.querySelector('.header'); // Здесь класс вашей шапки
-window.addEventListener('scroll', function() {
-    if (window.scrollY > 50) {
-    header.classList.addClass('.header__active'); // Добавить класс, который будет менять стили
-    }
-});
